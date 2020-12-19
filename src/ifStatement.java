47a3e6eb22f11473c3042225d53c6c13b6a911ae
Ozen <oturkoz@gmail.com>
2020-12-19 08:03:19 -0800

    create if statement file

---
+++ src/ifStatement.java
@@ -0,0 +1,45 @@
+public class ifStatement {
+    public static void main(String[] args){
+//        RELATIONAL OPERTORS
+//                > greater than
+//                >= greater than or equal
+//                < less than
+//                <= less than or equal
+//                == equal to   ** = is used to assign a value to the variables
+//                != not equal to
+//        if(true){ //needs to be true to run
+//            System.out.println("inside the curly brackets");
+//
+//    }
+//        int number = 6;
+//        if(number > 5) {
+//            System.out.println("The number is greater than 5");
+//
+//        }
+//        int number = 4;
+//        if(number > 5){
+//            System.out.println("The number is greater than 5");
+//            //this statement doesn't work since the number is < 5
+//        }
+//        System.out.println(number > 5);
+
+//        int number = 7;
+//        if(number == 3){ //doesn't run because inside the parenthesis is not true
+//            System.out.println("tne number is not equal to 7");
+//        }
+
+//        int number = 7;
+//        if(number != 3){
+//            System.out.println("The number is not equal to 7");
+//        }
+
+
+
+
+
+
+
+
+    }
+
+}

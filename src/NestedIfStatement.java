73361d4b6eb97b4d6a7913ca43bbf0e5859154ea
Ozen <oturkoz@gmail.com>
2020-12-19 08:22:56 -0800

    create nested is statement file

---
+++ src/NestedIfStatement.java
@@ -0,0 +1,32 @@
+public class NestedIfStatement {
+    // this is an if statement inside an if statement
+    public static void main (String[] args) {
+        //checking if a number is between 5 and 10
+//        int number = 7;
+//        if(number > 5){
+//            if(number < 10){
+//                System.out.println("The number is between 5 and 10");
+//            }
+//        }
+
+//        int number = 11;
+//        if(number > 5){
+//            if(number < 10){
+//                System.out.println("The number is between 5 and 10");
+//            }
+//        } // doesn't print anything since the number is not between 5 and 10
+
+//        int number = 11;
+//        if(number > 5){
+//            System.out.println("The number is greater than 5"); // only prints here
+//
+//            if(number < 10){
+//                System.out.println("The number is between 5 and 10"); // false, doesn't print
+//            }
+//        }
+
+
+
+
+    }
+}

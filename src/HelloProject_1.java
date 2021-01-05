6013353965f366e9865cab3fe1b7204ff59cf17e
oturkoz <cinaralti80>
2021-01-04 17:11:23 -0800

    Added println to Hello World

--- src/HelloProject_1.java
+++ src/HelloProject_1.java
@@ -1,5 +1,7 @@
 public class HelloProject_1 {
     public static void main (String[] args){
+       // System.out.println("Hello World");
+
         int number1 = 10;
         int number2 = 20;
         // float numberSum = (float) number1 / number2;
